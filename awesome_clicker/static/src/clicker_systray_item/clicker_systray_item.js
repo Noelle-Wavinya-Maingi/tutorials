9ff5e691c0e0f1837b19222c6df6d5dd1c50e758
Noelle <noemaingi@gmail.com>
2024-10-30 14:30:53 +0300

    :sparkles: added a dropdown to the systray item

--- awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
+++ awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
@@ -5,30 +5,50 @@ import { Component } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
 import { useClicker } from "../clicker_hook";
 import { ClickerValue } from "../clicker_value/clicker_value";
+import { Dropdown } from "@web/core/dropdown/dropdown";
+import { DropdownItem } from "@web/core/dropdown/dropdown_item";
 
-  export class ClickerSystray extends Component {
-    static template = "awesome_clicker.ClickerSystray";
-    static props = {};
-    static components = { ClickerValue };
+export class ClickerSystray extends Component {
+  static template = "awesome_clicker.ClickerSystray";
+  static props = {};
+  static components = { ClickerValue, Dropdown, DropdownItem };
 
-    setup() {
-        this.notification = useService("notification");
-        this.action = useService("action");
-        this.clicker = useClicker();
-    }
-
-    openClientAction() {
-        this.action.doAction({
-            type: "ir.actions.client",
-            tag: "awesome_clicker.client_action",
-            target: "new",
-            name: "Clicker Game",
-        });
-    }
+  setup() {
+    this.notification = useService("notification");
+    this.action = useService("action");
+    this.clicker = useClicker();
   }
 
-  export const systrayItem = {
-    Component: ClickerSystray,
-  };
+  openClientAction() {
+    this.action.doAction({
+      type: "ir.actions.client",
+      tag: "awesome_clicker.client_action",
+      target: "new",
+      name: "Clicker Game",
+    });
+  }
 
-  registry.category("systray").add("awesome_clicker ClickerStray", systrayItem, { sequence: 1000 });
+  get numberTrees() {
+    let sum = 0;
+    for (const tree in this.clicker.trees) {
+      sum += this.clicker.trees[tree].purchased;
+    }
+    return sum;
+  }
+
+  get numberFruits() {
+    let sum = 0;
+    for (const tree in this.clicker.trees) {
+      sum += this.clicker.trees[tree].purchased;
+    }
+    return sum;
+  }
+}
+
+export const systrayItem = {
+  Component: ClickerSystray,
+};
+
+registry
+  .category("systray")
+  .add("awesome_clicker ClickerStray", systrayItem, { sequence: 1000 });
